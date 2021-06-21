c406a112a5a7846fb4fb4d1c5383e3bfbb1ca439
0923kdh <0923kdh@gmail.com>
2021-06-22 02:29:13 +0900

    #16. 머스테치를 이용하여 기본페이지 생성

---
+++ freelec-springboot2-webservice-clone/src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -0,0 +1,20 @@
+package com.jojoldu.book.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/*
+머스테치 스타터 컨트롤러에서 문자열을 반환할 때 앞의 경로와 뒤의 파일 확장자는 자동으로 지정함.
+앞의 경로 src/main/resources/templates로, 뒤의 파일 확장자는 .mustache가 붙음.
+즉 여기선 index를 반환하므로 src/main/resources/templates/index.mustache로 전환되어
+View Resolver가 처리하게 된다.
+*/
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
